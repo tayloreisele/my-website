b4d3345c7a2feb2c288741c31053038c7243ce57
Taylor Eisele <tayloreisele@taylors-air.lan>
2023-06-12 23:21:27 -0500

    Optimized for mobile, and removed unneeded code.

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const hamburger = document.getElementById('hamburger');
-const navList = document.getElementById('nav-list');
-const socialMediaContainer = document.querySelector('.social-media-container');
-
-hamburger.addEventListener('click', () => {
-  navList.classList.toggle('show');
-  socialMediaContainer.classList.toggle('show');
-});
-
-
-
 // Auto-typing
 var typed = new Typed(".auto-type", {
   strings: [
