e56641282f057a45cac1f0853c18979855d8eb76
Taylor Eisele <tayloreisele@Taylors-MacBook-Air.local>
2023-06-16 21:24:36 -0500

    Added local time clock, removed old code, and updated padding.

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-// Auto-typing
+// Typed
 var typed = new Typed(".auto-type", {
-  strings: [
-    "Front end web developer.",
-    "Business leader.",
-    "Student.",
-  ],
-  typeSpeed: 150,
-  backSpeed: 150,
-  loop: true,
-});
+    strings: ["Front end developer.", "Business leader.", "Student."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+    });
+
+// Clock
+function currentTime() {
+    
+    let date = new Date();
+
+    
+    let options = { 
+        timeZone: 'America/Chicago',
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit' 
+    };
+
+    
+    let centralTime = new Intl.DateTimeFormat('en-US', options).format(date);
+
+    
+    let [hh, mm, ss] = centralTime.split(":");
+
+    hh = parseInt(hh, 10);
+    let session = hh >= 12 ? "PM" : "AM";
+
+    
+    hh = hh % 12;
+    hh = hh ? hh : 12;
+
+    
+    let time = `${hh}:${mm}:${ss} ${session}`;
+    document.getElementById("clock").innerText = time; 
+}
+
+setInterval(currentTime, 1000);
